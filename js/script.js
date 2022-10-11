6aad4a5f00bfa85b78f8f4f493192a409fdc86e1
magrega <proshanov@yandex.ru>
2022-10-11 08:37:32 +0300

    first commit of the second layout

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".header__burger").onclick = function() {
+    document.querySelector(".header__burger").classList.toggle("active");
+    document.querySelector(".header__navbar").classList.toggle("active");
+
+};
